5a71437c80cf290d99b52d8619631655e2150d5c
Jade McClain <99243434+rusteeez@users.noreply.github.com>
2026-04-27 16:25:24 -0700

    Update ConnectFour.cpp
    Added a way to keep score, added GetWinner()

--- ConnectFour.cpp
+++ ConnectFour.cpp
@@ -3,11 +3,12 @@
 using namespace std;
 
 // Constructor to initialize the game with two players
-ConnectFour::ConnectFour(Player* p1, Player* p2) : player1(p1), player2(p2) {}
+ConnectFour::ConnectFour(Player* p1, Player* p2) : player1(p1), player2(p2), winner('D') {}
 
 // Game loop
 void ConnectFour::PlayGame() {
     Player* currentPlayer = player1;
+    winner = 'D';
     while (true) {
         board.PrintBoard();
         int move = currentPlayer->MakeNextMove(board);
@@ -22,6 +23,7 @@ void ConnectFour::PlayGame() {
         if (board.CheckWin(currentPlayer->GetPlayerSymbol())) {
             board.PrintBoard();
             cout << "Player " << currentPlayer->GetPlayerSymbol() << " wins!" << endl;
+            winner = currentPlayer->GetPlayerSymbol();
             break;
         }
 
@@ -29,10 +31,15 @@ void ConnectFour::PlayGame() {
         if (board.FullBoard()) {
             board.PrintBoard();
             cout << "It's a draw!" << endl;
+            winner = 'D';
             break;
         }
 
         // Switch players
         currentPlayer = (currentPlayer == player1) ? player2 : player1;
     }
+}
+//Get winner score
+char ConnectFour::GetWinner() const {
+    return winner;
 }
